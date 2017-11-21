95ba1f601818082e2236b02bbbe5a5f2526c58ce
Caio L. Morceli <morcelicaio@hotmail.com>
2017-11-21 02:25:17 -0200

    Add files via upload

---
+++ public/javascripts/dropdown.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(function(){
+        $(".button-collapse").sideNav();
+    });
+    $(function(){
+        $("#btn-dropdown-perfil").dropdown({
+            belowOrigin: true
+        });
+    });
+    $(function(){
+        $("#btn-dropdown-perfilMobile").dropdown({
+            belowOrigin: true
+        });
+    });
+});
